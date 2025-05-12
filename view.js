461c3168969fe07d700c3084d18d0ca3b85783b9
Rebecka Kinn <rebeckaki@getacademy.no>
2025-05-12 15:21:05 +0200

    added files

---
+++ view.js
@@ -0,0 +1,43 @@
+
+
+updateView()
+function updateView(){
+    document.getElementById("app").innerHTML = /*HTML*/`
+    <header>
+    ${logo()}
+    ${header()}</header>
+    <main>
+    <h1>hallo</h1>
+    
+    </main>
+    <footer></footer>
+    `;
+}
+
+function logo(){
+     return /*HTML*/`
+    <div class="flexRow">
+        <img src="" alt="logo">
+        <p>noe kult</p>
+    </div>
+    `;
+}
+
+
+function header(){
+    return /*HTML*/`
+    <nav>
+        <ul class="removeBullet menu">
+            <li>Om SosNet</li>
+            <li>Aktuelt</li>
+            <li>Kunnskap og inspirasjon</li>
+            <li>Medlemmer</li>
+            <li>Kontakt</li>
+            <li>
+                <input type="text" placeholder="Søk">
+            </li>
+        </ul>
+    </nav>
+    
+    `;
+}
