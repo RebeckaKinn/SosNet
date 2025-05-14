b3299ced4d332e669c86756bd049cfdf75d7df17
Rebecka Kinn <rebeckaki@getacademy.no>
2025-05-14 15:23:23 +0200

    la til nye filer til nye sider - oppkobling funger ikke optimalt

---
+++ pages/aktuelt/script.js
@@ -0,0 +1,16 @@
+import {header} from "../../components/header/header.js"
+import {footer} from "../../components/footer/footer.js"
+
+window.header = header;
+window.footer = footer;
+
+renderLandingPage()
+function renderLandingPage(){
+    document.body.innerHTML = /*HTML*/`
+    ${header()}
+    <main role="main">
+        
+    </main>
+    ${footer()}
+    `;
+}
