64fe1cc3785006be3cd2698d41e542ec404e1a72
Rebecka Kinn <rebeckaki@getacademy.no>
2025-05-26 15:17:29 +0200

    trying to add a img banner

--- pages/om_SosNet/om_oss/script.js
+++ pages/om_SosNet/om_oss/script.js
@@ -10,10 +10,11 @@ function renderPage(){
     ${header()}
     <main role="main">
 
-        <section class="background-color-green banner">
-            <div>
-                text
-            </div>
+        <section class="background-color-green banner bottom-shadow">
+            <article class="main-margin flex column justify-content-center">
+                <h1>Om SosNet</h1>
+                <p>SosNet er et regionalt nettverk med aktører fra ulike sektorer som, gjennom sin ulikhet, vil  bidra med å øke sosial bærekraft i næringslivet, i offentlige tjenester og i sivilsamfunnet i Vestfold og Telemark.</p>
+            </article>
             <div class="banner-img">
                 <img src="/img/placeholders/ai_people_2.jpg" alt="">
             </div>
@@ -24,21 +25,22 @@ function renderPage(){
             <div class="banner-img">
                 <img src="/../../img/components/SOSNETfigurer.png" alt="">
             </div>
-            
             <article>
-                <h1>Om SosNet</h1>
-                <p><b>SosNet er et regionalt nettverk med aktører fra ulike sektorer som, gjennom sin ulikhet, vil  bidra med å øke sosial bærekraft i næringslivet, i offentlige tjenester og i sivilsamfunnet i Vestfold og Telemark.</b></p>
+                <div>
+                    <p>Det er ikke sant at like barn leker best. Det er kanskje harmonisk og stille da, men det er nok fordi de leker det de alltid har lekt. Når ulike barn kommer sammen kan det oppstå nye ideer, ny kunnskap i kombinasjoner av det hver av dem allerede vet, og nye løsninger kan gro fram, som hver av dem ikke hadde tenkt på. Det kan bli krangel og bråk av slikt, men ikke om ulikheten gror i tillit. SosNet skal derfor være et nettverk bygget på ulikhet, nysgjerrighet og tillit. </p>
+                    <p>På disse sidene vi vi spre kunnskap, dele gode eksempler, invitere til samlinger og bygge fellesskap om sosial bærekraft i regionen.</p>
+                </div>
             </article>
+            
         </section>
 
         <section class="main-margin">
             <article  class="grid-layout">
                 <div>
-                    <p>Det er ikke sant at like barn leker best. Det er kanskje harmonisk og stille da, men det er nok fordi de leker det de alltid har lekt. Når ulike barn kommer sammen kan det oppstå nye ideer, ny kunnskap i kombinasjoner av det hver av dem allerede vet, og nye løsninger kan gro fram, som hver av dem ikke hadde tenkt på. Det kan bli krangel og bråk av slikt, men ikke om ulikheten gror i tillit. SosNet skal derfor være et nettverk bygget på ulikhet, nysgjerrighet og tillit. </p>
-                    <p>På disse sidene vi vi spre kunnskap, dele gode eksempler, invitere til samlinger og bygge fellesskap om sosial bærekraft i regionen.</p>
+                    <p><b>Sosial bærekraft</b> handler både om å forebygge og reparere ulikhet i helse, urettferdighet, utenforskap og utrygghet i arbeidsliv og samfunnsliv. Det er ikke «de andres» ansvar – det er oss alles ansvar. Bli med, da vel!</p>
+                    
                 </div>
                 <div>
-                    <p><b>Sosial bærekraft</b> handler både om å forebygge og reparere ulikhet i helse, urettferdighet, utenforskap og utrygghet i arbeidsliv og samfunnsliv. Det er ikke «de andres» ansvar – det er oss alles ansvar. Bli med, da vel!</p>
                     <p>Tekst om medlemskap inn her. Priser, betingelser, fordeler, etc. </p>
                 </div>
             </article>
