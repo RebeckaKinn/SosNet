2590284d123ab6f2fea40fb4c46f3c2e13655f93
Rebecka Kinn <rebeckaki@getacademy.no>
2025-05-27 11:33:57 +0200

    fixing the contact page

--- pages/kontakt/script.js
+++ pages/kontakt/script.js
@@ -10,17 +10,47 @@ function renderPage(){
     document.body.innerHTML = /*HTML*/`
     ${header()}
     <main role="main">
-        
+        <div class="gradient-line top gradient-background"></div>
     <section class="grid-layout">
         <section class="contact-img-section">
             <div class="img-container">
 
                 <img src="/img/placeholders/default_img.jpg" alt="">
             </div>
-            <div class="gradient-line gradient-background"></div>
+           
+        </section>
+        <section class="main-margin">
+            <h1 class="">Kotakt oss</h1>
+            <address class="flex column flex-gap-1rem">
+                <ul class="flex column flex-gap-05rem">
+                    <li>
+                       <h3>Lars U. Kobro</h3>
+                       <i>SosNet koordinator og seniorforsker</i>
+                    </li>
+                    <li>
+                        <a class="font-responsive" href="mailto:kobro@usn.no">kobro@usn.no</a>
+                    </li>
+                    <li>
+                        <a class="font-responsive" href="tel:+4790942561">+47 909 42 561</a>
+                    </li>
+                </ul>
+            </address>
+        </section>
+
+        <section class="main-margin text-align-right">
+            <h1>Finn oss</h1>
+            <address class="flex column flex-gap-1rem">
+                <ul class="flex column flex-gap-05rem font-responsive">
+                    <li>
+                        <p><b  class="font-responsive">Universitetet I Sørøst-Norge</b></p>
+                        <p><i>Senter for sosialt entreprenørskap og samskapende sosial innovasjon, SESAM.</i></p>
+                    </li>
+                    <li>Kjølnes ring 56</li>
+                    <li>3918 Porsgrunn</li>
+                </ul>
+            </address>
         </section>
         <section>
-            <h1>Kotakt oss</h1>
             <div>${mapIframe}</div>
         </section>
     </section>
