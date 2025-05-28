66ec51b23f01a2cbb5227acc77c7552b56467428
Rebecka Kinn <rebeckaki@getacademy.no>
2025-05-28 13:19:13 +0200

    made a placeholder for pages that is not done

---
+++ components/placeholder/placeholder.js
@@ -0,0 +1,29 @@
+
+
+export function placeholder(){
+    return /*HTML*/`
+        <section>
+
+            <section class="landingPageVideo landingPageImageContainer background-color-green bottom-shadow" aria-live="polite">
+                <video class="frontPageVideo" playsinline autoplay muted loop>
+                    <source src="/video/placeholder/buissness_working.mp4" type="video/mp4">
+                </video>
+            
+                <div class="overlay"></div>
+                <div class="overlayText">
+                    <h1 class="main-color-3">Under oppbygging</h1>
+                    <p class="font-responsive">Denne siden er under oppbygging. Vi jobber med å få innholdet på plass her.</p>
+                    <p class="font-responsive">Takk for tålmodigheten din!</p>
+                </div>
+
+            </section>
+            <section class="main-margin flex column justify-content-center align-items-center">
+               <h2>Spørsmål?</h2>
+               <p class="text-align-center">Har du spørsmål om denne siden eller trenger mer informasjon? Ta gjerne kontakt med oss – vi hjelper deg gjerne!</p>
+               <button>
+                    <a href="/pages/kontakt/index.html">Kontakt oss</a>
+                </button>
+            </section>
+        </section>
+    `;
+}
